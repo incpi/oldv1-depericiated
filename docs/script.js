47552b0961b09fecafd8b6fa12fcd2fa782e84b0
Omkar Patel <87596092+incpi@users.noreply.github.com>
2023-12-12 18:59:10 +0000

    check test 1

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+document.querySelector('#imgloop').innerHTML= ()=>{
+    let x=""
+    i=0
+while (i<28){
+    x+="<img src='images/XAMTA Tutorial-"+i+".png ></img></img>"
+    i++}
+    return x
+}
