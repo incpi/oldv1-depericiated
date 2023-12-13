dfcfd169807188d3ea2512ba0a44b56221f1a61b
Omkar Patel <87596092+incpi@users.noreply.github.com>
2023-12-13 09:29:35 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
  let x="";
- let i=0
-while (i<28){
+ let i=1
+while (i<=28){
     x+=`<img src='images/XAMTA Tutorial-${i}.png' class='ui fluid inage'></img>`;
     i++}
 document.querySelector('#imgloop').innerHTML=x;
