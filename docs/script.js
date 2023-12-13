c59311737ec5a7ea61f368f54eafbe02fa2952ed
Omkar Patel <87596092+incpi@users.noreply.github.com>
2023-12-13 04:58:37 +0000

    menu

--- docs/script.js
+++ docs/script.js
@@ -6,3 +6,32 @@ while (i<28){
     i++}
     return x
 }
+document.querySelector('#h2use').innerHTML=()=>{
+   x=`<pre >
+   Step 1 - download a zip file with the extension
+   If you already have downloaded a file with the plugin - skip this step.
+
+   If you don't have a file, but have a link to a github repository - follow the link, then click the big green "Clone or download" button, then click Download ZIP.
+
+   Step 2 - extract the contents of the zip file
+   Right click on the downloaded zip file, then click "Extract Here".
+
+   Step 3 - open the extension page in google chrome
+   There are several ways todo that.
+
+   Option 1: type chrome://extensions in the url bar and press enter.
+
+   Option 2: click on the tree dots in the top right of the browser, then click "More tools" then click "Extensions".
+
+   Step 4 - activate developer mode
+   Turn on the switch on the top right of the page that says "Developer mode";
+
+   Step 5 - load unpacked extension
+   Click on the button on the top left of the page that says "Load unpacked".
+
+   Then select a folder that contains the manifest.json file.
+   </pre>`
+   
+    return x
+}
+$('.menu .item').tab();
