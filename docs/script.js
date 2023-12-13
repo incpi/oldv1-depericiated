e6c890cacf729cb75bd4f25bd9f2738a6c8deb6f
Omkar Patel <87596092+incpi@users.noreply.github.com>
2023-12-13 09:18:22 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
- let x=""
-    i=0
+ let x="";
+ let i=0
 while (i<28){
-    x+=`<img src='docs/images/XAMTA Tutorial-${i}.png ></img>`;
+    x+=`<img src='docs/images/XAMTA Tutorial-${i}.png' ></img>`;
     i++}
-document.querySelector('#imgloop').innerHTML=x
+document.querySelector('#imgloop').innerHTML=x;
