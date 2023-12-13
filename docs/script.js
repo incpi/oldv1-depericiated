689371f483e468c76bf5ab32d1fe9f72d7d89045
Omkar Patel <87596092+incpi@users.noreply.github.com>
2023-12-13 09:27:40 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
  let x="";
  let i=0
 while (i<28){
-    x+=`<img src='docs/images/XAMTA Tutorial-${i}.png' ></img>`;
+    x+=`<img src='images/XAMTA Tutorial-${i}.png' class='ui fluid inage'></img>`;
     i++}
 document.querySelector('#imgloop').innerHTML=x;
