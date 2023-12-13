7ae62196d10ba4f7a6caf6366aef651ecbfb63cc
Omkar Patel <87596092+incpi@users.noreply.github.com>
2023-12-13 23:49:04 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,45 @@
-function genimgloop(){
+function pc(){
+x='<br><br><pre>
+This extension will help you to enter marks with ease.
+
+Step 1.  
+Install the extension
+    op1: use store and get it from there.
+    op2: download from GitHub and use directly.
+
+Step 2. 
+Once you enable this extension, you will get float button at right bottom corner. with greyscale icon simillar to extension page icon. and once you hover over it you will get orange icon theme color option.
+
+Step 3.
+click on this extension  it will open a popup which contains data to enter marks.
+if your page is blank, nothing wrong here. It's because we didn't setup anything yet. enter your relevant information in Xamta bot similar what we do in mobile.
+
+once you select exam and subject in chat bot.
+you will get data with your details.
+verify and correct if needed in chatbot.
+
+Step 4. 
+All the data on screen is visible.
+you can see students name there id's, make them present or absent for this exam.
+
+step 5.  
+Enter mark questionwise. if you enter mark raw color will change it to blue. then at the end of same raw. click on save. it will save data. once data is save button will be disabled.
+if it fails you can also retry.
+
+Step 6. 
+if you enter marks for multiple students. then all blue raws will be got save by save all button at bottom
+
+
+<+>-- Color Legends  --<+>
+Red : Error state
+Blue : Ready state
+Green : Success state
+yellow : Warning state
+
+</pre>'
+document.querySelector('#pc').innerHTML+=x
+}
+document.querySelector('#h2use').innerHTML=xfunction genimgloop(){
  let x="";
 
  let i=1
